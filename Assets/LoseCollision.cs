296819c28f37ff68204359e7a9980dd497ee0bea
Fabutan <30437917+Fabutan@users.noreply.github.com>
2018-05-07 22:29:29 -0500

    Added Lose Collider

---
+++ Assets/LoseCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print("trigger");
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		print("Collision");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
